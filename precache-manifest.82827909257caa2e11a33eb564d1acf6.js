4740ba6124ea2e7b96d005a98bb9831ee6bd725d
Mysqyrlex <alf9915@yandex.ru>
2020-09-23 16:13:49 +0500

    Updates

---
+++ precache-manifest.82827909257caa2e11a33eb564d1acf6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48394415440b33dbf3d2d55414df8a95",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e3331ff60bf1beab416d",
+    "url": "./static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "3e719b31cc90a56504ab",
+    "url": "./static/css/main.b226b730.chunk.css"
+  },
+  {
+    "revision": "e3331ff60bf1beab416d",
+    "url": "./static/js/2.82df3316.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.82df3316.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e719b31cc90a56504ab",
+    "url": "./static/js/main.dae6b23b.chunk.js"
+  },
+  {
+    "revision": "f8dbc2b3edf0fd274ca0",
+    "url": "./static/js/runtime-main.fc70c573.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a8f06966af89009a59251b93517df3d2.js"
+  "./precache-manifest.82827909257caa2e11a33eb564d1acf6.js"
 );
 
 self.addEventListener('message', (event) => {
