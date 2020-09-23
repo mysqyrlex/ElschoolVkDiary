5b1987827147ba18db31056c10d7ed3699e4e9d7
Mysqyrlex <alf9915@yandex.ru>
2020-09-23 14:30:44 +0500

    Updates

---
+++ precache-manifest.3683a69d32d9622cfc0764b48d399ed5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1ff0f89c3d32851b9a31f330d911273",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1537c41ab85d275990b3",
+    "url": "./static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "8718e163a25106dae7b8",
+    "url": "./static/css/main.657c8d72.chunk.css"
+  },
+  {
+    "revision": "1537c41ab85d275990b3",
+    "url": "./static/js/2.699dd53e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.699dd53e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8718e163a25106dae7b8",
+    "url": "./static/js/main.4fc489b1.chunk.js"
+  },
+  {
+    "revision": "f8dbc2b3edf0fd274ca0",
+    "url": "./static/js/runtime-main.fc70c573.js"
+  }
+]);
